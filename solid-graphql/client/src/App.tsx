9a09ed98a4294ce3c6cf323f55de8b5ff83f7d2b
Jake Lee <40153724+jlee0425@users.noreply.github.com>
2021-09-07 01:46:45 +0900

    urql

--- solid-graphql/client/src/App.tsx
+++ solid-graphql/client/src/App.tsx
@@ -1,27 +1,102 @@
-import type { Component } from "solid-js";
+import {
+	createClient,
+	defaultExchanges,
+	subscriptionExchange,
+} from '@urql/core';
+import { Component, createResource, createSignal, For } from 'solid-js';
+import { SubscriptionClient } from 'subscriptions-transport-ws';
 
-import logo from "./logo.svg";
-import styles from "./App.module.css";
+const subscriptionClient = new SubscriptionClient('ws://localhost:4000', {
+	reconnect: true,
+});
+const client = createClient({
+	url: 'http://localhost:4000',
+	exchanges: [
+		...defaultExchanges,
+		subscriptionExchange({
+			forwardSubscription: (operation) =>
+				subscriptionClient.request(operation) as any,
+		}),
+	],
+});
+
+const [todos, { refetch }] = createResource(() =>
+	client
+		.query(
+			`query {
+        getTodos {
+          id
+          done
+          text
+        }
+      }
+  `,
+		)
+		.toPromise()
+		.then(({ data }) => data.getTodos),
+);
 
 const App: Component = () => {
-  return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
-    </div>
-  );
+	const [text, setText] = createSignal('');
+	const onAdd = async () => {
+		await client
+			.mutation(
+				`
+      mutation($text: String!) {
+        addTodo(text: $text) {
+          id
+        }
+      }
+    `,
+				{ text: text() },
+			)
+			.toPromise()
+			.then(() => {
+				refetch();
+				setText('');
+			});
+	};
+	const toggleTodo = async (id: string) => {
+		await client
+			.mutation(
+				`
+      mutation($id: ID!, $done: Boolean!) {
+        setDone(id: $id, done: $done) {
+          id
+        }
+      }
+    `,
+				{ id, done: !todos().find((todo) => todo.id === id) },
+			)
+			.toPromise()
+			.then(() => {
+				// refetch();
+			});
+	};
+	return (
+		<div>
+			<For each={todos()}>
+				{({ id, done, text }) => (
+					<div>
+						<input
+							type='checkbox'
+							checked={done}
+							onclick={() => toggleTodo(id)}
+						/>
+						{text}
+					</div>
+				)}
+			</For>
+			<div>
+				<input
+					type='text'
+					value={text()}
+					oninput={(evt) => setText(evt.currentTarget.value)}
+				/>
+				<button onclick={onAdd}>Add</button>
+			</div>
+		</div>
+	);
 };
 
 export default App;
